d75400a1874e74992fbbc35d927b22acee47a9c1
Henry <hnr.leto11@gmail.com>
2020-10-31 13:39:26 -0400

    controller bugs fixed

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,11 @@
 class FlightsController < ApplicationController
   def index
-    @flights = Flight.where(from_airport_id: params[:search][:from_airport],
-                            to_airport_id: params[:search][:to_airport], start: Date.parse(params[:search][:date]).all_day)
-                            # the query works when 'start' is removed
+    if params[:search]
+      @flights = Flight.where(from_airport_id: params[:search][:from_airport],
+                              to_airport_id: params[:search][:to_airport], start: Date.parse(params[:search][:date]).all_day)
+                              # the query works when 'start' is removed
+    else
+      @flights = Flight.all
+    end
   end
 end
