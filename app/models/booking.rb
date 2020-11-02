cdd5f4bddd00caa17202c47bdf6f42ae2d720b00
Henry <hnr.leto11@gmail.com>
2020-11-02 11:42:05 -0500

    complete flight booking passenger relationship

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,8 @@
 class Booking < ApplicationRecord
-  belongs_to :flights
-  belongs_to :passengers
+  belongs_to :flight
+  belongs_to :passenger
 
-  # validates :number_of_passengers, length: { minumum: 1, maximum: 9 }
+  # accept_nested_attributes_for :passengers
+
+  validates :number_of_passengers, length: { minumum: 1, maximum: 9 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_02_153313) do
+ActiveRecord::Schema.define(version: 2020_11_02_163729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,13 +37,11 @@ ActiveRecord::Schema.define(version: 2020_11_02_153313) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "passengers"
-    t.integer "booking_id"
   end
 
   create_table "passengers", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.integer "booking_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
