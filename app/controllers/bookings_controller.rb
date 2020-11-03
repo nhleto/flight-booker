ab57e629d62b92733200228c721d40efcaa68c80
Henry <hnr.leto11@gmail.com>
2020-11-03 12:01:15 -0500

    finalizing booking form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,15 +2,15 @@ class BookingsController < ApplicationController
   def new
     @booking = Booking.new
     @flight = Flight.find_by(id: params[:booking][:flight_id])
+    @passenger = Passenger.new
     passenger_forms = params[:booking][:number_of_passengers].to_i
     passenger_forms.times do
-      @passengers = Passenger.new
+      @passenger.bookings.build
     end
   end
 
   def create
     @booking = Booking.new(booking_params)
-    @passengers = Passenger.new(booking_params)
     if @booking.save
       flash[:notice] = 'Booking successfully saved'
       redirect_to root_path
@@ -23,6 +23,6 @@ class BookingsController < ApplicationController
   private
 
   def booking_params
-    params.require(:booking).permit(:passenger_id, :flight_id, :number_of_passengers, :from_airport, :to_airport, :date, :passenger)
+    params.require(:booking).permit(:id, :number_of_passengers, passenger_attributes: %i[id name email])
   end
 end
